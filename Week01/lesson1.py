c15fdba2cf55028ae66cc737b1405f586a914ba1
Timothy White <thwhite2009@yahoo.com>
2026-07-05 14:35:42 -0400

    Complete Lesson 1

---
+++ Week01/lesson1.py
@@ -0,0 +1,17 @@
+greeting = "Hello!"
+name = "Timothy"
+age = 28
+favorite_team = "New York Knicks"
+city = "Rangeley"
+job = "Roofing Company Owner"
+favorite_card_brand = "Panini"
+learning = "Python"
+
+print(greeting)
+print("\nMy name is", name + ".")
+print("\nI am", age, "years old.")
+print("\nI live in", city + ".")
+print("\nMy favorite team is the", favorite_team + "!")
+print("\nI work as a", job + ".")
+print("\nMy favorite card brand is", favorite_card_brand + ".")
+print("\nI am currently learning", learning + "!")
