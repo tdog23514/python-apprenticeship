fb73861b32d4e4da2fc62f6c5e0e5cce7da430ad
Timothy White <thwhite2009@yahoo.com>
2026-07-06 21:32:35 -0400

    Add project RoofingEstimateCalculator.py

---
+++ RoofingEstimateCalculator.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+city = input("What city do you live in? ")
+roof_size = input("What is the size of your roof in square feet? ")
+print("Estimate Summary \nCustomer:", name, "\nCity:", city, "\nRoof Size:", roof_size, "sq ft.")
