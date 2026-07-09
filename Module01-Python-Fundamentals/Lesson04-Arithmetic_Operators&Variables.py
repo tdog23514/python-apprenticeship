d1b5c0c125172b7277bcd28acaf01b3e9b4656ce
Timothy White <thwhite2009@yahoo.com>
2026-07-08 21:38:11 -0400

    Add project Lesson04-Arithmetic_Operators&Variables.py

---
+++ Module01-Python-Fundamentals/Lesson04-Arithmetic_Operators&Variables.py
@@ -0,0 +1,15 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2 
+
+print("\nResults")
+print("-" *10)
+
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
