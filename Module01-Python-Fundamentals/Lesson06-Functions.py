58d77161f74ef0eaa5e73f68e979634226e13c22
Timothy White <thwhite2009@yahoo.com>
2026-07-12 12:29:21 -0400

    Add project Lesson06-Functions.py

---
+++ Module01-Python-Fundamentals/Lesson06-Functions.py
@@ -0,0 +1,18 @@
+def greet():
+    print("Welcome to Python!")
+
+greet()
+greet()
+greet()
+
+def favorite_team(team):
+    print("Your favorite team is the", team)
+
+favorite_team("Knicks")
+favorite_team("Ravens")
+favorite_team("Yankees")
+
+def multiply(length, width):
+    return length * width
+roof_area = multiply(40, 32)
+print(roof_area)
